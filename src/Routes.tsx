49f8aeeb038b8279cb20770973d3a521a9f71136
Adrian Sudbury <sudburya@gmail.com>
2021-03-12 16:25:23 +0000

    update to routes

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -2,9 +2,6 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import HomePage from "../src/pages/HomePage";
 import ProcessPage from "./pages/ProcessPage";
 import CasePage from "./pages/CasePage";
-import ErrorPage from "./components/ErrorPage";
-import PageNotFound from "./components/PageNotFound";
-import CoreSetup from "./components/setup/CoreSetup";
 import { logInfo } from "../src/utils/Logger";
 
 export default function Routes(): JSX.Element {
@@ -14,9 +11,6 @@ export default function Routes(): JSX.Element {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/ruleau" component={HomePage} />
-        <Route exact path="/setup" component={CoreSetup} />
-        <Route exact path="/ruleau/setup" component={CoreSetup} />
-
         <Route exact path="/process/:processId" component={ProcessPage} />
         <Route exact path="/process/:processId/cases" component={ProcessPage} />
         <Route exact path="/process/:processId/rules" component={ProcessPage} />
@@ -31,9 +25,6 @@ export default function Routes(): JSX.Element {
           path="/process/:processId/case/:caseId"
           component={CasePage}
         />
-        <Route exact path="/error" component={ErrorPage} />
-        <Route exact path="/ruleau/error" component={ErrorPage} />
-        <Route component={PageNotFound} />
       </Switch>
     </BrowserRouter>
   );
